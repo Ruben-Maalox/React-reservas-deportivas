ff598bcbc7a89d21687311819c6c91d2f78bf4c5
Ruben Ferre Martinez <maal0x1234@gmail.com>
2024-04-21 18:12:06 +0200

    feat: update page reservations

--- src/components/auth/ReservationsTable.tsx
+++ src/components/auth/ReservationsTable.tsx
@@ -10,61 +10,110 @@ export default function ReservationsTable() {
   const [showRegistro, setShowRegistro] = useState<boolean>(false); // Nuevo estado para controlar la visualización del componente de registro
 
   useEffect(() => {
-    
     fetch("http://localhost:8000/api/instalaciones/all")
       .then((response) => response.json())
       .then((data) => setInstalaciones(data.instalaciones));
-  }, [])
-  
-  
-  
+  }, []);
+
   const handleClick = (hour: number) => {
     // Comprobar aquí inicio de sesión
 
     // Si NO HAY sesión iniciada, mostrar el componente de inicio/registro
     setShowRegistro(true);
-    setUrlInfo(`?hour=${hour}`)
+    setUrlInfo(`?hour=${hour}`);
 
     // Si hay sesión iniciada, redireccionar a /reservas/pistas con la info en la URL
 
     // pasar parámetros por URL
     // history.push(`/reservas/pistas/${hour}`);
   };
-  
-  if(token !== ""){
+
+  if (token !== "") {
     // Que nos lleve a --> ConfirmarReserva.tsx
     console.log("El token es" + token);
     console.log("REDIRECCIONAME A ConfirmarReserva");
   }
 
-  console.log(urlInfo)
+  console.log(urlInfo);
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
+  const handlePrevDay = () => {
+    setSelectedDate(
+      (prevDate) => new Date(new Date(prevDate).setDate(prevDate.getDate() - 1))
+    );
+  };
+
+  const handleNextDay = () => {
+    setSelectedDate(
+      (prevDate) => new Date(new Date(prevDate).setDate(prevDate.getDate() + 1))
+    );
+  };
+  // const today = new Date();
+  const formattedDate = `${selectedDate.getFullYear()}-${String(
+    selectedDate.getMonth() + 1
+  ).padStart(2, "0")}-${String(selectedDate.getDate()).padStart(2, "0")}`;
   return (
     <>
       {instalaciones.length > 0 && !showRegistro && (
-        <table className="bg-white w-full text-center">
-          <thead>
-            <tr>
-              <th>Horas</th>
-              {instalaciones.map((instalacion)=>{
-                return <th key={instalacion.id}>{instalacion.nombre}</th>
-              })}
-            </tr>
-          </thead>
-          <tbody>
-            {Array.from({ length: 24 }, (_, i) => i).map((hour) => (
-              <tr key={hour} onClick={() => handleClick(hour)}>
-                <td>{`${hour}:00 - ${hour + 1}:00`}</td>
-                <td>Libre</td>
-                <td>Libre</td>
-                <td>Libre</td>
-                <td>Libre</td>
+        <div className="w-full md:w-2/3 lg:w-3/4 mx-auto bg-white rounded-lg p-4">
+          <h1 className="font-bold text-center text-3xl mb-5">Reservas</h1>
+          <div className="flex bg-gray-200 items-center p-3">
+            <label
+              htmlFor="reservation-date"
+              className="block text-sm font-medium text-gray-700 mr-3"
+            >
+              Fecha:
+            </label>
+            <button
+              onClick={handlePrevDay}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold m-1 px-3 rounded"
+            >
+              &lt;
+            </button>
+            <input
+              type="date"
+              id="reservation-date"
+              value={formattedDate}
+              className="mt-1 block pl-3 pr-5 sm:text-sm border-gray-300 rounded-md"
+            />
+            <button
+              onClick={handleNextDay}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold m-1 px-3 rounded"
+            >
+              &gt;
+            </button>
+          </div>
+          <table className=" w-full text-center">
+            <thead>
+              <tr>
+                {instalaciones.map((instalacion) => {
+                  return (
+                    <th key={instalacion.id} className="p-4">
+                      {instalacion.nombre}
+                    </th>
+                  );
+                })}
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {Array.from({ length: 29 }, (_, i) => i + 18).map((halfHour) => {
+                const hour = Math.floor(halfHour / 2) % 24;
+                const time = `${hour}:${halfHour % 2 === 0 ? "00" : "30"}`;
+                return (
+                  <tr key={halfHour}>
+                    {instalaciones.map((instalacion) => (
+                      <td key={instalacion.id} className="border">
+                        {time}
+                      </td>
+                    ))}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       )}
-      {showRegistro && token === "" && <Login setToken={setToken}/>}
+      {showRegistro && token === "" && <Login setToken={setToken} />}
       {token !== "" && <p className="bg-white">Redireccionando...</p>}
     </>
   );

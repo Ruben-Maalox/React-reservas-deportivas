d80943d11f503d5459d98a52eabd01f29d2277ca
Juan <juan.albaladejo.pay@gmail.com>
2024-04-22 21:33:11 +0200

    feat: add instalaciones, servicios page

--- src/pages/Instalaciones.tsx
+++ src/pages/Instalaciones.tsx
@@ -1,8 +1,38 @@
-export default function Instalaciones(){
-  return(
-    <>
-    <h1>Esta es la página INSTALACIONES</h1>
-    <img src="../assets/images/figma/instalaciones.png" alt="" />
-    </>
-  )
+import baloncestoImg from "../assets/images/instalaciones/baloncesto.jpg";
+import futbolSalaImg from "../assets/images/instalaciones/futbol-sala.jpg";
+import padelImg from "../assets/images/instalaciones/pista-padel.png";
+import piscinaImg from "../assets/images/instalaciones/piscina.jpg";
+import tenisImg from "../assets/images/instalaciones/tenis.jpg";
+import futbol7Img from "../assets/images/instalaciones/futbol7.jpg";
+import volleyballImg from "../assets/images/instalaciones/volleyball.jpg";
+
+export default function Instalaciones() {
+  const images = [
+    { src: baloncestoImg, alt: "Pista de baloncesto" },
+    { src: futbolSalaImg, alt: "Pista de futbol sala" },
+    { src: padelImg, alt: "Pista de padel" },
+    { src: piscinaImg, alt: "Piscina" },
+    { src: tenisImg, alt: "Pista de tenis" },
+    { src: futbol7Img, alt: "Campo de futbol 7" },
+    { src: volleyballImg, alt: "Pista de volleyball" },
+  ];
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 m-4">
+        {images.map((image, index) => (
+          <div key={index} className="overflow-hidden shadow-lg rounded-lg h-80 w-64 cursor-pointer m-auto">
+            <img alt={image.alt} src={image.src} className="w-full h-64 object-cover" />
+            <div className="p-4">
+              <h1 className="text-xl font-bold">{image.alt}</h1>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
+
+/* TODO
+- Mejorar la optimización de las imágenes (tardan mucho en cargar)
+*/

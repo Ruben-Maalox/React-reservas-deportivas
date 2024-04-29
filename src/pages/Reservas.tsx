d5827cb970c8712d12615149a35ce9821f7932a8
Juan <1883216@alu.murciaeduca.es>
2024-04-29 20:23:54 +0200

    refactor: change route of a component

--- src/pages/Reservas.tsx
+++ src/pages/Reservas.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import backgroundImage from "../assets/images/fondo-reservas.jpg";
-import ReservationsTable from "../components/auth/ReservationsTable";
+import ReservationsTable from "../components/ReservationsTable";
 import OwnReservations from "../components/OwnReservations";
+import "./Reservas.css"
 
 export default function Reservas() {
   const [showOwnReservations, setShowOwnReservations] = useState<boolean>(false);
@@ -11,7 +12,7 @@ export default function Reservas() {
   };
   return (
     <>
-      <main style={{ backgroundImage: `url(${backgroundImage})` }} className="flex min-h-screen flex-col items-center p-24 bg-gray-100 text-black bg-no-repeat bg-cover">
+      <main style={{ backgroundImage: `url(${backgroundImage})` }} className="relative flex min-h-screen flex-col items-center p-24 bg-gray-100 text-black bg-no-repeat bg-cover">
         <div className="absolute top-5 right-5 inline-block w-10 mr-2 align-middle select-none transition duration-500 ease-in">
           <input type="checkbox" name="toggle" id="toggle" checked={showOwnReservations} onChange={handleCheckboxChange} className="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" />
           <label htmlFor="toggle" className="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>

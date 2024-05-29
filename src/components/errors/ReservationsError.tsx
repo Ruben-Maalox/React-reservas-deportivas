b03c4e6d1fb090c0774acbb7567e7bcdc6e105e9
Juan <juan.albaladejo.pay@gmail.com>
2024-05-29 17:12:50 +0200

    feat: change error position in ReservationsError

--- src/components/errors/ReservationsError.tsx
+++ src/components/errors/ReservationsError.tsx
@@ -1,11 +1,10 @@
 export default function ReservationsErrors() {
   return (
-    <div
-      className="absolute top-0 right-0 m-6 p-4 bg-red-100 border border-red-400 text-red-700 rounded-lg shadow-lg w-1/6"
-      role="alert"
-    >
-      <strong className="font-bold block mb-2">Error!</strong>
-      <span className="block sm:inline">No puedes reservar esa pista!</span>
+    <div className="fixed inset-0 flex items-center justify-center z-30">
+      <div className="m-6 p-4 bg-red-100 border border-red-400 text-red-700 rounded-lg shadow-lg w-auto" role="alert">
+        <strong className="font-bold block mb-2">Error!</strong>
+        <span className="block sm:inline">No puedes reservar esa pista!</span>
+      </div>
     </div>
   );
 }

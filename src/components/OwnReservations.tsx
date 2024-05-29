2ef0a63a75302f07bd8daf8265ed86a65113f8f0
Juan <juan.albaladejo.pay@gmail.com>
2024-05-29 17:40:54 +0200

    feat: add pagination to ownReservations table

--- src/components/OwnReservations.tsx
+++ src/components/OwnReservations.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 // import reservationsJSON from "../json_prueba/reservas.json";
 // --> Types
 import { EditReservationInfo, Instalacion, Reserva } from '../types/types';
@@ -23,7 +23,10 @@ export default function OwnReservations({
   const { user } = useAuthProvider();
   const [editInfo, setEditInfo] = useState<EditReservationInfo | null>(null);
   const [date, setDate] = useState<Date>();
+  const [showAllReservations, setShowAllReservations] = useState<boolean>(false);
   const [showEditReservation, setShowEditReservation] = useState<boolean>(false);
+  const totalReservations = useRef<number>(0);
+  const [pagination, setPagination] = useState<number>(1);
 
   // Cada vez que pulsamos en el botón de editar y cambiamos sus valores, se ejecuta para mostrar el modal de edición
   useEffect(() => {
@@ -34,8 +37,24 @@ export default function OwnReservations({
   }, [editInfo]);
 
   const ownReservations = useMemo(() => {
-    return reservations?.filter((reservation) => reservation.idUsuario === user?.id).sort((a, b) => b.id - a.id);
-  }, [reservations]);
+    if (showAllReservations) {
+      const allReservations = reservations
+        ?.filter((reservation) => reservation.idUsuario === user?.id)
+        .sort((a, b) => b.id - a.id);
+
+      totalReservations.current = allReservations?.length || 0;
+      setPagination(1);
+
+      return allReservations;
+    }
+
+    totalReservations.current = reservations?.length || 0;
+    setPagination(1);
+
+    return reservations
+      ?.filter((reservation) => new Date(reservation.fechaYHora).getTime() >= new Date().getTime())
+      .sort((a, b) => b.id - a.id);
+  }, [reservations, showAllReservations]);
 
   const filteredInstallation = useMemo(() => {
     return installations?.filter((instalacion) => instalacion.id === editInfo?.installationId);
@@ -46,6 +65,16 @@ export default function OwnReservations({
       return reservations?.filter((reservation) => reservation.idInstalacion === editInfo?.installationId);
     }
   }, [reservations, editInfo]);
+
+  const handlePagination = (next: boolean) => {
+    if (next) {
+      if (totalReservations.current <= pagination * 10) return;
+      setPagination((prevState) => prevState + 1);
+    } else {
+      if (pagination === 1) return;
+      setPagination((prevState) => prevState - 1);
+    }
+  };
 
   const deleteReservation = async (reservationId: number) => {
     try {
@@ -88,103 +117,129 @@ export default function OwnReservations({
             editInfo={editInfo}
           />
         ) : (
-          <table className="min-w-full divide-y divide-gray-200">
-            <thead className="bg-gray-100">
-              <tr>
-                <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  ID Reserva
-                </th>
-                <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  ID Usuario
-                </th>
-                <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Instalacion
-                </th>
-                <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Fecha
-                </th>
-                <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Hora
-                </th>
-                <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Duración
-                </th>
-                <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Importe
-                </th>
-                <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Acciones
-                </th>
-              </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-200">
-              {ownReservations &&
-                (ownReservations.length === 0 ? (
-                  <tr key={1}>
-                    <td colSpan={8} className="text-center text-bold">
-                      No tienes ninguna reserva aún!
-                    </td>
-                  </tr>
-                ) : (
-                  ownReservations.map((reserva, index) => {
-                    const { id, idUsuario, idInstalacion, fechaYHora, duracion, importe } = reserva;
-                    const nombreInstalacion = installations?.find(
-                      (instalacion) => instalacion.id === idInstalacion,
-                    )?.nombre;
-                    const [date, timeWithZone] = fechaYHora.split('T');
-                    const [time, _] = timeWithZone.split('+');
+          <>
+            <div className="mb-2">
+              <button
+                onClick={() => setShowAllReservations((prevState) => !prevState)}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              >
+                {showAllReservations ? 'Mostrar actuales' : 'Mostrar todas'}
+              </button>
+            </div>
 
-                    const reservationDateTime = new Date(`${date}T${time}`);
-                    const differenceInHours =
-                      (reservationDateTime.getTime() -
-                        new Date().getTime() +
-                        (reservationDateTime.getTimezoneOffset() + 120) * 60 * 1000) /
-                      (1000 * 60 * 60);
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-100">
+                <tr>
+                  <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    ID Reserva
+                  </th>
+                  <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    ID Usuario
+                  </th>
+                  <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Instalacion
+                  </th>
+                  <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Fecha
+                  </th>
+                  <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Hora
+                  </th>
+                  <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Duración
+                  </th>
+                  <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Importe
+                  </th>
+                  <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Acciones
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {ownReservations &&
+                  (ownReservations.length === 0 ? (
+                    <tr key={1}>
+                      <td colSpan={8} className="text-center text-bold">
+                        No tienes ninguna reserva aún!
+                      </td>
+                    </tr>
+                  ) : (
+                    ownReservations.slice(pagination * 10 - 10, pagination * 10).map((reserva, index) => {
+                      const { id, idUsuario, idInstalacion, fechaYHora, duracion, importe } = reserva;
+                      const nombreInstalacion = installations?.find(
+                        (instalacion) => instalacion.id === idInstalacion,
+                      )?.nombre;
+                      const [date, timeWithZone] = fechaYHora.split('T');
+                      const [time, _] = timeWithZone.split('+');
 
-                    return (
-                      <tr key={index} className="hover:bg-gray-100">
-                        <td className="px-6 py-4 whitespace-nowrap text-center">{id}</td>
-                        <td className="px-6 py-4 whitespace-nowrap text-center">{idUsuario}</td>
-                        <td className="px-6 py-4 whitespace-nowrap text-center" data-id={idInstalacion}>
-                          {nombreInstalacion}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-center">{date}</td>
-                        <td className="px-6 py-4 whitespace-nowrap text-center">{time}</td>
-                        <td className="px-6 py-4 whitespace-nowrap text-center">{duracion}</td>
-                        <td className="px-6 py-4 whitespace-nowrap text-center">{importe}€</td>
-                        <td className="flex flex-row px-6 py-4 whitespace-nowrap text-center">
-                          <div className="flex pl-4 justify-center">
-                            {differenceInHours > 12 && (
-                              <>
-                                <img
-                                  src="src/assets/icons/edit.svg"
-                                  alt="Edit icon"
-                                  className="w-8 cursor-pointer"
-                                  onClick={() =>
-                                    setEditInfo({
-                                      installationId: idInstalacion,
-                                      date: reservationDateTime,
-                                      reservationId: id,
-                                    })
-                                  }
-                                />
+                      const reservationDateTime = new Date(`${date}T${time}`);
+                      const differenceInHours =
+                        (reservationDateTime.getTime() -
+                          new Date().getTime() +
+                          (reservationDateTime.getTimezoneOffset() + 120) * 60 * 1000) /
+                        (1000 * 60 * 60);
 
-                                <img
-                                  src="src/assets/icons/delete.svg"
-                                  alt="Delete icon"
-                                  className="w-10 cursor-pointer"
-                                  onClick={() => deleteReservation(id)}
-                                />
-                              </>
-                            )}
-                          </div>
-                        </td>
-                      </tr>
-                    );
-                  })
-                ))}
-            </tbody>
-          </table>
+                      return (
+                        <tr key={index} className="hover:bg-gray-100">
+                          <td className="px-6 py-4 whitespace-nowrap text-center">{id}</td>
+                          <td className="px-6 py-4 whitespace-nowrap text-center">{idUsuario}</td>
+                          <td className="px-6 py-4 whitespace-nowrap text-center" data-id={idInstalacion}>
+                            {nombreInstalacion}
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap text-center">{date}</td>
+                          <td className="px-6 py-4 whitespace-nowrap text-center">{time}</td>
+                          <td className="px-6 py-4 whitespace-nowrap text-center">{duracion}</td>
+                          <td className="px-6 py-4 whitespace-nowrap text-center">{importe}€</td>
+                          <td className="flex flex-row px-6 py-4 whitespace-nowrap text-center">
+                            <div className="flex pl-4 justify-center">
+                              {differenceInHours > 12 && (
+                                <>
+                                  <img
+                                    src="src/assets/icons/edit.svg"
+                                    alt="Edit icon"
+                                    className="w-8 cursor-pointer"
+                                    onClick={() =>
+                                      setEditInfo({
+                                        installationId: idInstalacion,
+                                        date: reservationDateTime,
+                                        reservationId: id,
+                                      })
+                                    }
+                                  />
+
+                                  <img
+                                    src="src/assets/icons/delete.svg"
+                                    alt="Delete icon"
+                                    className="w-10 cursor-pointer"
+                                    onClick={() => deleteReservation(id)}
+                                  />
+                                </>
+                              )}
+                            </div>
+                          </td>
+                        </tr>
+                      );
+                    })
+                  ))}
+              </tbody>
+            </table>
+
+            <div className="flex justify-center">
+              <button
+                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-4"
+                onClick={() => handlePagination(false)}
+              >
+                Previous
+              </button>
+              <button
+                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => handlePagination(true)}
+              >
+                Next
+              </button>
+            </div>
+          </>
         )}
       </div>
     </>
